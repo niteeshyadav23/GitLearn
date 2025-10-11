b74a27cb3620017db59f3d4916b81afdf9b29c4c
Nitish Yadav <divocstuff19@gmail.com>
2025-10-11 12:49:37 +0530

    Update the HomePage

--- GitLearningVue/src/Components/HomePage.vue
+++ GitLearningVue/src/Components/HomePage.vue
@@ -1,3 +1,11 @@
 <template>
     <h1>Homepage</h1>
+    <p>This is the home page and i will now add home page to default page</p>
     </template>
+
+
+<script>
+export default {
+    name:'Home',
+};
+</script>
